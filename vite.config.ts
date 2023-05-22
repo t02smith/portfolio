5f4a8529f1d68a2141fd69d5b032dd451a5f6dc1
Tom Smith <thomas-smith@sky.com>
2023-05-22 16:12:58 +0100

    built version added

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
