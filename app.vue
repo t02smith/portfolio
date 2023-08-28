673bf54d19c93b303dec3ab701c8100e3bda7fc1
Tom Smith <thomas-smith@sky.com>
2023-08-29 00:20:43 +0100

    added basic page meta

--- app.vue
+++ app.vue
@@ -3,3 +3,19 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<script setup>
+useHead({
+  title: "My site",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    {
+      name: "description",
+      content: "A portfolio site containing lots of information about myself.",
+    },
+  ],
+  link: [
+    { rel: "icon", type: "image/png", href: "/icons/projects/this-site.png" },
+  ],
+});
+</script>
